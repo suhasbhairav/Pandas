fb609480780026f0367b181d99582873ed5f1302
Suhas Bhairav <suhasbhairav@gmail.com>
2020-12-31 18:52:14 +0100

    Date Ranges
    Signed-off-by: Suhas Bhairav <suhasbhairav@gmail.com>

---
+++ practise16.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+
+print(pd.date_range("1/1/2010", periods=5))
+
+print(pd.date_range("1/1/2145", periods=10))
+
+print(pd.bdate_range("1/1/2021", periods=10))
